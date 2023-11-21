76840e09d2b479046ea914cda51793526b24d9f6
rc0fficial <to.arsalanali@gmail.com>
2023-11-22 04:42:16 +0500

    update on tag

--- src/components/common/svg/Bin.svg.jsx
+++ src/components/common/svg/Bin.svg.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Bin() {
   return (
- <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-trash" width={22} height={22} viewBox="0 0 24 24" strokeWidth="1.5" stroke="#2c3e50" fill="none" strokeLinecap="round" strokeLinejoin="round">
+ <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-trash" width={22} height={22} viewBox="0 0 24 24" strokeWidth="1.5" stroke="#fff" fill="none" strokeLinecap="round" strokeLinejoin="round">
   <path stroke="none" d="M0 0h24v24H0z" fill="none" />
   <path d="M4 7l16 0" />
   <path d="M10 11l0 6" />
--- src/components/pdf/PDFViewer.jsx
+++ src/components/pdf/PDFViewer.jsx
@@ -2,9 +2,8 @@ import React, { useState } from "react";
 import { Document, Page } from "react-pdf";
 import Draggable from "react-draggable";
 import Bin from "../common/svg/Bin.svg";
-import { pdfjs } from 'react-pdf';
+import { pdfjs } from "react-pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-
 
 const PDFViewer = () => {
   const [selectedFile, setSelectedFile] = useState(null);
@@ -20,7 +19,7 @@ const PDFViewer = () => {
   };
 
   return (
-    <div className="flex">
+    <div className="flex mt-4 ml-4">
       <div>
         <input type="file" onChange={handleFileChange} />
         {selectedFile && (
@@ -36,9 +35,13 @@ const PDFViewer = () => {
       </div>
       <div className="">
         <Draggable>
-          <div className="cursor-move flex items-center justify-center gap-2 border bg-red-500  shadow-md rounded py-2 px-2">
-            Testing
-            <Bin />
+          <div className="flex items-center justify-center border border-gray-300 rounded bg-gray-400 ">
+            <div className="cursor-move flex items-center text-white justify-center gap-2  bg-red-500  shadow-md rounded py-2 px-2">
+              Testing
+            </div>
+            <div className="cursor-pointer bg-gray-400 h-full">
+              <Bin />
+            </div>
           </div>
         </Draggable>
       </div>
