43d3f406d976494a87b5ac9ae94e0ed828821787
rc0fficial <to.arsalanali@gmail.com>
2023-11-22 02:35:11 +0500

    update

--- src/components/pdf/index.jsx
+++ src/components/pdf/index.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
-import Draggable from 'react-draggable';
+import React, { useState } from "react";
+import Draggable from "react-draggable";
+import Bin from "../common/svg/Bin.svg";
 
 export default function Pdf() {
+  const [pdfFile, setPdfFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setPdfFile(file);
+  };
+
+  const handleRemovePdf = () => {
+    setPdfFile(null);
+  };
+
   return (
-    <div>
-      Pdf page
-      <Draggable>
-  <div className='pointer'>I can now be moved around!</div>
-</Draggable>
+    <div className="flex items-center justify-center mt-10">
+      <div className="">
+        <input
+          type="file"
+          accept=".pdf"
+          onChange={handleFileChange}
+          className="mb-2"
+        />
+        <Draggable>
+          <div className="cursor-move">
+            {pdfFile && (
+              <div className="mb-2 flex items-center justify-center border gap-4 shadow-md rounded py-2">
+                {pdfFile.name}
+                <span
+                  className="ml-2 text-red-500 cursor-pointer"
+                  onClick={handleRemovePdf}
+                >
+                  <Bin />
+                </span>
+              </div>
+            )}
+          </div>
+        </Draggable>
+      </div>
     </div>
-  )
+  );
 }
